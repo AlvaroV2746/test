1d61f1fca54db4d9c6850271e5c266cbcfd872c9
Alvaro Villarreal <alvarovillarreal2746@hotmail.com>
2026-08-05 18:48:01 -0500

    sequenceSum.js Done

---
+++ sequenceSum.js
@@ -0,0 +1,14 @@
+const sequenceSum = (begin, end) => {
+    if (begin > end) {
+        return NaN;
+    }
+
+    if (begin === end) {
+        return begin;
+    }
+
+    return begin + sequenceSum(begin + 1, end);
+};
+ 
+console.log(sequenceSum(1, 5));
+ 
