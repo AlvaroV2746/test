ca62de027f7cc7ad500c7f0a25580cc7f7d54afd
Alvaro Villarreal <alvarovillarreal2746@hotmail.com>
2026-08-10 07:25:34 -0500

    Genertor.js Done

---
+++ generator.js
@@ -0,0 +1,69 @@
+// @ts-check
+
+
+// BEGIN (write your solution here)
+const tree = {
+    name: 'nodejs-package',
+    type: 'directory',
+    meta: { hidden: true },
+    children: [
+        {
+            name: 'README.md',
+            type: 'file',
+            meta: {},       
+        },
+        {
+            name: 'dist ',
+            type: 'directory',
+            meta: {},       
+            children: [],
+        },
+        {
+            name: '__tests__  ',
+            type: 'directory',
+            meta: {},       
+            children: [
+                {
+                    name: 'half.test.js',
+                    type: 'file',
+                    meta: { type: 'text/javascript' },       
+                }
+            ],
+        },
+        {
+            name: 'babel.config.js',
+            type: 'file',
+            meta: { type: 'text/javascript' },       
+        },
+        {
+            name: 'node_modules',
+            type: 'directory',
+            meta: { owner: 'root', hidden: false },       
+            children: [
+                {
+                    name: '@babel',
+                    type: 'directory',
+                    meta: {},       
+                    children: [
+                        {
+                            name: 'cli ',
+                            type: 'directory',
+                            meta: {},       
+                            children: [
+                                {
+                                    name: 'LICENSE ',
+                                    type: 'file',
+                                    meta: {},       
+                                }
+                            ],
+                        }
+
+                    ],
+                }
+            ],
+        }
+    ],
+};
+
+
+// END
