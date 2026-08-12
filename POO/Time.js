3c32348873f8dec65ba797d81108887454786109
Alvaro Villarreal <alvarovillarreal2746@hotmail.com>
2026-08-12 17:32:13 -0500

    Time DONE

--- POO/Time.js
+++ POO/Time.js
@@ -2,7 +2,17 @@
 
 export default class Time {
   // BEGIN (write your solution here)
-  
+    toString() {
+    return `${this.hours}:${this.minutes}`;
+  }
+  static fromString(time){
+    const timeSplit = time.split(":");
+    const newTime = new Time(timeSplit[0], timeSplit[1]);
+    console.log(newTime);
+    
+    return newTime;
+  }
+
   // END
 
   constructor(hours, minutes) {
@@ -10,7 +20,15 @@ export default class Time {
     this.hours = hours;
   }
 
-  toString() {
-    return `${this.hours}:${this.minutes}`;
-  }
 }
+const time = new Time(10, 15);
+console.log(`La hora es ${time}`); // => 'La hora es 10:15'
+
+
+const time1 = Time.fromString('10:23');
+// el método toString() se llama automáticamente
+console.log(time1);
+
+console.log(`La hora es ${time1}`); // 'La hora es 10:23'
+
+
